fcbe991905c0557323f784ad6e7165fed4ff3eea
Usuario <user@example.com>
2026-04-09 22:29:13 -0500

    bajar paquetes a net8

--- Program.cs
+++ Program.cs
@@ -2,42 +2,33 @@ global using ReservasHotelApi.Models;
 global using ReservasHotelApi.Data;
 global using ReservasHotelApi.DTOs;
 
-using Scalar.AspNetCore;
 using Microsoft.EntityFrameworkCore;
-using System.Text;
 using ReservasHotelApi.Repositories;
 using ReservasHotelApi.Services;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Servicios
-builder.Services.AddOpenApi();
-builder.Services.AddControllers();
+builder.Services.AddControllers(); // 👈 faltaba esto
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<HotelContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Registra Repository y Service
-builder.Services.AddScoped(typeof(Repository<>), typeof(Repository<>)); // 👈 genérico
+builder.Services.AddScoped(typeof(Repository<>), typeof(Repository<>));
 builder.Services.AddScoped<HabitacionService>();
 builder.Services.AddScoped<HuespedService>();
 builder.Services.AddScoped<ReservaService>();
 
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.MapScalarApiReference();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-
 app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
